5791793fca93bb9514daa6b892dadaf5bee84b1c
changstudent <akscjfals@naver.com>
2022-09-12 19:39:07 +0900

    Virtualizing memory

--- os.c
+++ os.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
 #include <assert.h>
 #include <Windows.h>    // Sleep(1000) 를 쓰기 위한 헤드함수
 
 int main (int argc, char *argv[]) { // (입력 받는 파라메타 갯수, 값)
-    if (argc != 2) {
-        fprintf(stderr, "usage: cpu <string>\n");   // 에러메시지
-        exit(1);
-    }
-    char *str = argv[1];
+    int *p = malloc(sizeof(int));
+    assert(p != NULL);
+    printf("(%d) memory address of p: %p\n", getpid(), p);
+    *p = 0;
+
     while (1) {
-        Sleep(4000);   // 1초 기다림
-        printf("%s\n", str);
+        Sleep(2000);   // 1초 기다림
+        *p = *p + 1;
+        printf(" (%d) p : %d\n", getpid(), *p);
     }
     return 0;
 }
